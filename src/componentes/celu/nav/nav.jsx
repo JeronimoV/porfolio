a23b920ba1060a4f1b39b1d8f1c554c777edadc0
bobardou <jeronimoevilar@outlook.com>
2023-01-27 20:34:31 -0300

    ultimo

--- src/componentes/celu/nav/nav.jsx
+++ src/componentes/celu/nav/nav.jsx
@@ -7,7 +7,7 @@ function Nav(){
             <ul className={styles.header}>
                 <li><img className={styles.logo} src={logo} alt="" /></li>
                 <li><h1>Porfolio</h1></li>
-                <li><button onClick={window.location.href = "https://drive.google.com/file/d/1BUQ68zJrOqxKJ2Ns-QRQNAz8zvJC55HO/view?usp=sharing"} className={styles.boton}>Download CV</button></li>
+                <li><button onClick={() => window.location.href = "https://drive.google.com/file/d/1BUQ68zJrOqxKJ2Ns-QRQNAz8zvJC55HO/view?usp=sharing"} className={styles.boton}>Download CV</button></li>
                 </ul>
         </div>
     )
