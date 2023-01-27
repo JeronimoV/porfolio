2fcce66ee117e5b31164214119760e210a7454dd
bobardou <jeronimoevilar@outlook.com>
2023-01-27 20:39:08 -0300

    ahorasiperra

--- src/componentes/celu/nav/nav.jsx
+++ src/componentes/celu/nav/nav.jsx
@@ -2,12 +2,16 @@ import logo from "./logo.png"
 import styles from "./styles/nav.module.css"
 
 function Nav(){
+
+    function teievomami(){
+        window.location.href = "https://drive.google.com/file/d/1BUQ68zJrOqxKJ2Ns-QRQNAz8zvJC55HO/view?usp=sharing"
+    }
     return (
         <div className={styles.contenedor}>
             <ul className={styles.header}>
                 <li><img className={styles.logo} src={logo} alt="" /></li>
                 <li><h1>Porfolio</h1></li>
-                <li><button onClick={() => window.location.href = "https://drive.google.com/file/d/1BUQ68zJrOqxKJ2Ns-QRQNAz8zvJC55HO/view?usp=sharing"} className={styles.boton}>Download CV</button></li>
+                <li><button onClick={() => teievomami()} className={styles.boton}>Download CV</button></li>
                 </ul>
         </div>
     )
